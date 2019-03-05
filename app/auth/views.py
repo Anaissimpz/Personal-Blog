5253e2531ec36e1ae8d83706b6ec4abfecd30a29
Anaissimpz <anaissimpenzwe@gmail.com>
2019-03-05 08:22:36 +0200

    add login form function in auth's views.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -14,7 +14,6 @@ def login():
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
-
         flash('Invalid username or Password')
 
     title = "watchlist login"
